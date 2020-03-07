8a27c50c83f4407e952201220e513be8c52097ed
k.gavrilove <k.gavrilove@gmail.com>
2020-03-07 19:42:21 +0300

    Errors Page

---
+++ RecipeApp/Views/Errors/SomethingWentWrongPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecipeApp.Views.Errors {
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class SomethingWentWrongPage : ContentPage {
+    public SomethingWentWrongPage() {
+      InitializeComponent();
+    }
+  }
+}
