b5295d620f4848c80e61817cc5f6cc9b7185fb68
Dmitriy Osipov <Diabloks9@gmail.com>
2020-03-08 22:29:04 +0300

    Временно, тестирую работу с бд

--- RecipeApp/Views/AboutPage.xaml.cs
+++ RecipeApp/Views/AboutPage.xaml.cs
@@ -13,7 +13,6 @@ namespace RecipeApp.Views
         public AboutPage()
         {
             InitializeComponent();
-            string test;
         }
     }
 }
--- RecipeApp/Views/Account/SingUpPage.xaml.cs
+++ RecipeApp/Views/Account/SingUpPage.xaml.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.Specialized;
 
 namespace RecipeApp.Views {
   [XamlCompilation(XamlCompilationOptions.Compile)]
   public partial class SingUpPage : ContentPage {
     public SingUpPage() {
-      InitializeComponent();
+            InitializeComponent();
+    }
+
+    public void SignUp_Clicked(object sender, EventArgs e){ //Временно, тест работы с бд
+            if(login.Text != "" && password.Text != "" && email.Text != "" && password.Text == confirmPass.Text)
+            {
+                WebClient client = new WebClient();
+                Uri uri = new Uri("http://10.0.2.2/CreateUser.php");
+                NameValueCollection parameters = new NameValueCollection();
+
+                parameters.Add("user_login", login.Text);
+                parameters.Add("user_password", password.Text);
+                parameters.Add("email", email.Text);
+
+                var response = client.UploadValues(uri, "POST" ,parameters);
+                client.UploadValuesCompleted += client_UploadValuesComplited;
+                string json = Encoding.UTF8.GetString(response);
+                login.Text = json;
+            }
+    }
+
+    void client_UploadValuesComplited(object sender, UploadValuesCompletedEventArgs e){ //Пока временно, тестирую работу бд
+            //string id = Encoding.UTF8.GetString(e.Result);
+            //int newID = 0;
+
+            //int.TryParse(id, out newID);
     }
   }
 }
